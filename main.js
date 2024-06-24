fb8429d484eb197e9171cb17662b784d5f7a2c1e
mrrob <mr.robik12345@gmail.com>
2024-06-24 14:27:08 +0300

    it can find last messages

--- main.js
+++ main.js
@@ -2,9 +2,12 @@
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
     ]
 });
+
 const id = "!";
 require('dotenv').config();
 const token = process.env.DISCORD_BOT_TOKEN;
@@ -14,9 +17,7 @@ client.on("ready", () => {
 
     const link = client.generateInvite({
         permissions: [
-            PermissionFlagsBits.SendMessages,
-            PermissionFlagsBits.ManageGuild,
-            PermissionFlagsBits.MentionEveryone,
+            PermissionFlagsBits.Administrator
         ],
         scopes: [OAuth2Scopes.Bot],
     });
@@ -25,7 +26,7 @@ client.on("ready", () => {
 
 
 
-client.on("messageCreate", msg => {
+client.on("messageCreate",  async (msg) => {
     console.log(msg.content.startsWith(id));
     if (msg.author.bot || !msg.content.startsWith(id)) return;
     const args = msg.content.slice(id.length).split(/ +/);
@@ -33,7 +34,58 @@ client.on("messageCreate", msg => {
     console.log(cmd);
     
     if (cmd === "ping") {
-        msg.reply(`pong!\n ${Date.now() - msg.createdTimestamp}ms`);
+        msg.reply(`pong!\n${Date.now() - msg.createdTimestamp}ms`);
+    }
+    
+    else if (cmd === "check") {
+        if (args.length === 1) {
+            return;
+        } 
+        if (args.length === 0) {
+            const channel = msg.channel;
+            const endDate = new Date();
+            const startDate = new Date();
+            startDate.setDate(endDate.getDate() - 8);
+            const messages = fetchMessagesWithinDateRange(channel, startDate, endDate);
+            console.log(endDate);
+            console.log(startDate);
+            (await messages).forEach(message => {
+                console.log(message.content);
+            })
+        }
     }
 })
+
+async function fetchMessagesWithinDateRange(channel, startDate, endDate) {
+    let messages = new Map();
+    let lastID;
+    
+    for (let i = 0; i < 3; i++) {
+        const options = { limit: 100 };
+        if (lastID) {
+            options.before = lastID;
+        }
+        
+        const fetchedMessages = await channel.messages.fetch(options);
+        
+        if (fetchedMessages.size === 0) {
+            break;
+        }
+        
+        fetchedMessages.forEach(msg => {
+            if (msg.createdTimestamp >= startDate.getTime() && msg.createdTimestamp <= endDate.getTime()) {
+                messages.set(msg.id, msg);
+            }
+        })
+        
+        lastID = fetchedMessages.last().id;
+
+        if (fetchedMessages.size < 100) {
+            break;
+        }
+    }
+    
+    return messages;
+}
+
 client.login(token)
